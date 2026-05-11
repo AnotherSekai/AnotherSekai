04a585a8c2a00cd86badfb49a12b0569422b74ce
Darren Lau Wei Jie <darrenwjlau@gmail.com>
2026-05-11 08:47:04 +0800

    Change data fetch URL to sekai-world.github.io
    Updated the URL and referer for fetching data.

---
+++ api/sekai-world/[...path].js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+  const pathParam = req.query.path;
+
+  // normalize to array safely
+  const pathArray = Array.isArray(pathParam)
+    ? pathParam
+    : typeof pathParam === "string"
+      ? [pathParam]
+      : [];
+
+  const path = pathArray.join("/");
+
+  const url = `https://sekai-world.github.io/${path}`;
+
+  const response = await fetch(url, {
+    headers: {
+    }
+  });
+
+  const data = await response.arrayBuffer();
+
+  res.setHeader(
+    "Content-Type",
+    response.headers.get("content-type") || ""
+  );
+
+  res.status(response.status).send(Buffer.from(data));
+}
